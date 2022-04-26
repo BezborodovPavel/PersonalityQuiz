1b0f57c08940ffa74c0724aa4d883214f2b3238e
BezborodovPavel <bezborodovp@gmail.com>
2022-04-26 11:18:16 +0500

    Initial Commit

---
+++ PersonalityQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Alexey Efimov on 30.08.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var discriptLabel: UILabel!
+    
+    var answers: [Answer] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
+        
+        guard let favoriteAnimal = selectFavorite() else {return}
+        titleLabel.text = "Вы - \(favoriteAnimal.rawValue)"
+        discriptLabel.text = favoriteAnimal.definition
+    }
+    
+    private func selectFavorite() -> Animal?{
+        let counts = answers.reduce(into: [:]) {count, answer in
+            count[answer.animal, default: 0] += 1
+        }
+        let favoriteAnimal = counts.max{a, b in a.value < b.value}?.key
+        return favoriteAnimal
+    }
+}
